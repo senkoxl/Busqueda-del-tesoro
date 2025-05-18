652a0abd168e72ad41c12531a71b9dbabcadd414
senkoxl <senko.daar@gmail.com>
2025-05-17 19:25:04 -0500

    arreglo de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import vercel from "@astrojs/vercel/static";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "static",
-  adapter: vercel(),
 });
